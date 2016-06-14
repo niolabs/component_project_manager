1dd68ef4b465d653c1a3cf94ec371a12e275e9ec
mattdodge <matt@n.io>
2016-06-14 16:12:19 -0700

    Apply new web module refactoring NIO-901

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,62 +1,52 @@
-import threading
-from unittest.mock import Mock
-from nio.modules.security.user import User
-from nio.modules.web.http import Request
-from niocore.testing.test_case import NIOCoreTestCase
+from unittest.mock import MagicMock
+from nio.testing.modules.security.module import TestingSecurityModule
+from nio.modules.web.http import Request, Response
 
 from ..handler import ProjectManagerHandler
 from ..manager import ProjectManager
+from niocore.testing.web_test_case import NIOCoreWebTestCase
 
 
-class TestProjectManagerHandler(NIOCoreTestCase):
+class TestProjectManagerHandler(NIOCoreWebTestCase):
 
-    def get_test_modules(self):
-        return super().get_test_modules() | {'security'}
-
-    def setUp(self):
-        super().setUp()
-        setattr(threading.current_thread(), "user", User("tester"))
-
-    def tearDown(self):
-        delattr(threading.current_thread(), "user")
-        super().tearDown()
-
-    def _disable_security(self, handler):
-        handler.commands = []
+    def get_module(self, module_name):
+        # Don't want to test permissions, use the test module
+        if module_name == 'security':
+            return TestingSecurityModule()
+        else:
+            return super().get_module(module_name)
 
     def test_on_get(self):
         project_manager = ProjectManager()
-        project_manager.get_blocks_structure = Mock(return_value={})
+        project_manager.get_blocks_structure = MagicMock(return_value={})
         handler = ProjectManagerHandler("", project_manager)
-        self._disable_security(handler)
 
-        params = {"identifier": "blocks"}
-        request = Request(None, params, None)
-        response = Mock()
+        request = MagicMock(spec=Request)
+        request.get_params.return_value = {"identifier": "blocks"}
+        response = MagicMock(spec=Response)
         handler.on_get(request, response)
         project_manager.get_blocks_structure.assert_called_with()
 
     def test_on_get_invalid_params(self):
         project_manager = ProjectManager()
         handler = ProjectManagerHandler("", project_manager)
-        self._disable_security(handler)
 
-        params = {"identifier": "invalid_identifier"}
-        request = Request(None, params, None)
-        response = Mock()
+        request = MagicMock(spec=Request)
+        request.get_params.return_value = {"identifier": "invalid id"}
+        response = MagicMock(spec=Response)
         with self.assertRaises(ValueError):
             handler.on_get(request, response)
 
     def test_on_delete(self):
         project_manager = ProjectManager()
-        project_manager.remove_blocks = Mock(return_value={})
+        project_manager.remove_blocks = MagicMock(return_value={})
         handler = ProjectManagerHandler("", project_manager)
-        self._disable_security(handler)
 
         params = {"identifier": "blocks",
                   "twitter, filter": ""}
-        request = Request(None, params, None)
-        response = Mock()
+        request = MagicMock(spec=Request)
+        request.get_params.return_value = params
+        response = MagicMock(spec=Response)
         handler.on_delete(request, response)
         project_manager.remove_blocks.assert_called_with(
             ['twitter', 'filter'])
@@ -65,34 +55,26 @@ class TestProjectManagerHandler(NIOCoreTestCase):
         params = {"identifier": "blocks",
                   "twitter": "",
                   "filter": ""}
-        request = Request(None, params, None)
-        response = Mock()
+        request = MagicMock(spec=Request)
+        request.get_params.return_value = params
+        response = MagicMock(spec=Response)
         handler.on_delete(request, response)
-        # work around dict keys unpredictability
-        args_ok = False
-        try:
-            project_manager.remove_blocks.assert_called_with(['twitter',
-                                                              'filter'])
-            args_ok = True
-        except:
-            pass
-        try:
-            project_manager.remove_blocks.assert_called_with(['filter',
-                                                              'twitter'])
-            args_ok = True
-        except:
-            pass
-        self.assertTrue(args_ok)
+        # Don't know what order we will process the dictionary keys,
+        # make sure both blocks were removed and nothing more
+        call_args_removed = project_manager.remove_blocks.call_args[0][0]
+        self.assertIn('twitter', call_args_removed)
+        self.assertIn('filter', call_args_removed)
+        self.assertEqual(len(call_args_removed), 2)
 
     def test_on_delete_invalid_params(self):
         project_manager = ProjectManager()
-        project_manager.remove_blocks = Mock(return_value={})
+        project_manager.remove_blocks = MagicMock(return_value={})
         handler = ProjectManagerHandler("", project_manager)
-        self._disable_security(handler)
 
         params = {"identifier": "invalid_identifier",
                   "twitter, filter": ""}
-        request = Request(None, params, None)
-        response = Mock()
+        request = MagicMock(spec=Request)
+        request.get_params.return_value = params
+        response = MagicMock(spec=Response)
         with self.assertRaises(ValueError):
             handler.on_delete(request, response)
